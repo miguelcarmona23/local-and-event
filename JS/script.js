96d6bb6e6c00cf1f4ddeedf24e6bf4190186777c
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-10 13:27:50 -0500

    Added function addItem and EventListener

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,9 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e) {
+    console.log('Hello');
+}
+
+addItems.addEventListener('submit', addItem);
